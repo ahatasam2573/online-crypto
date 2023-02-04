2d02af0acdb489d319c62c452de4aab696db4c96
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-02-04 05:56:40 -0500

    components and routes created

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <p>hedaer</p>
+        </div>
+    );
+};
+
+export default Header; 
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './Header';
+export { default as Home } from './Home';
+export { default as Coins } from './Coins';
+export { default as Exchanges } from './Exchanges';
+export { default as CoinDetails } from './CoinDetails';
